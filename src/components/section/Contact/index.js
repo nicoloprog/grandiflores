72bbbda6bd5328ac55375d7b467678db0c73be88
nicoloprog <nicoloprog@gmail.com>
2023-09-21 13:58:19 -0400

    section Contact

--- src/components/section/Contact/index.js
+++ src/components/section/Contact/index.js
@@ -4,7 +4,46 @@ import styles from "./Contact.module.scss";
 const Contact = () => {
   return (
     <div className={styles.component}>
-      <div className={styles.contact}></div>
+      <div className={styles.contact}>
+        <div className={styles.footer}>
+          <div className={styles.top}>
+            <h2>Quota</h2>
+            <p>
+              "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis,
+              accusantium?"
+            </p>
+            <ul className={styles.icons}>
+              <ion-icon className={styles.icon} name="logo-facebook"></ion-icon>
+              <ion-icon
+                className={styles.icon}
+                name="logo-instagram"
+              ></ion-icon>
+              <ion-icon className={styles.icon} name="logo-twitter"></ion-icon>
+              <ion-icon className={styles.icon} name="logo-youtube"></ion-icon>
+            </ul>
+          </div>
+          <div className={styles.aside}>
+            <ul className={styles.category}>
+              <li>
+                <h3>Project</h3>
+              </li>
+              <li>Houses</li>
+              <li>Rooms</li>
+              <li>Flats</li>
+              <li>Apartments</li>
+            </ul>
+            <ul className={styles.category}>
+              <li>
+                <h3>Company</h3>
+              </li>
+              <li>Objective</li>
+              <li>Capital</li>
+              <li>Security</li>
+              <li>Selling</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
